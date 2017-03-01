6cbeb3cf9484e6d4961000b6e29832842563b64a
oxyyyyy <elektronikaion@gmail.com>
2017-03-01 14:58:06 +0200

    Added animation

--- js/main.js
+++ js/main.js
@@ -12,7 +12,12 @@ $(document).ready(function() {
     $.ajax({
       type: 'POST',
       url: $(form).attr('action'),
-      data: formData
+      data: formData,
+      beforeSend: function () {
+        // $(formStatus).removeClass('error text-danger animated bounceIn');
+        // $(formStatus).addClass('processing');
+        $(formStatus).html('<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>');
+      }
     })
 
     .done(function(response) {
